84ec97770b86ae3403a476a26f7e661a5e6fad98
Pietro Ramos <pietroramos5@gmail.com>
2024-12-05 21:08:14 -0300

    vercel config to deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
